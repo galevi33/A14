13a039fa17c4873e172bf771e6838c6d3436716b
galevi33 <galevi33@gmail.com>
2023-06-26 15:28:59 +0300

    the end

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,48 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
 
 @Component({ templateUrl: 'home.component.html' })
-export class HomeComponent { }
+export class HomeComponent {
+    constructor(private httpClient: HttpClient) { }
+    //public jsonObject: JSON;
+    public  pokemonList: object[] = [];
+      ExtendedPokemonList: Array<any> = [];
+    ngOnInit() {
+        console.log("start");
+
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=18').then(res => res.json()).then(
+            async res => {
+
+                this.pokemonList = res.results;
+                this.pokemonList.forEach(element => {
+                    console.log(element);
+                });
+                await this.bulidTable();
+                var a="";
+            });
+    }
+
+
+    async bulidTable() {
+
+       await this.pokemonList.forEach(element => {
+
+            let pok = element as any;
+
+            fetch(pok.url).then(res => res.json()).then(
+
+                res => {
+
+                    let name = res.name;
+                    let photo = res.sprites.front_default;
+                    this.ExtendedPokemonList.push({ name: name, photo: photo });
+
+                });
+
+        });
+
+
+    }
+
+}
